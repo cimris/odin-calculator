cde02c0a0e317d4fce8b3eb01e4446a773b7e850
cimris <77905334+cimris@users.noreply.github.com>
2022-03-24 17:28:49 -0600

    added event key listener

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,72 @@
 //My Scripts
+//Key Press Listener
+window.addEventListener("keydown", function (event) {
+  switch (event.key) {
+    case "Backspace":
+      console.log(event.key);
+      break;
+    case "Delete":
+      console.log(event.key);
+      break;
+    case "(":
+      console.log(event.key);
+      break;
+    case ")":
+      console.log(event.key);
+      break;
+    case "%":
+      console.log(event.key);
+      break;
+    case "/":
+      console.log(event.key);
+      break;
+    case "*":
+      console.log(event.key);
+      break;
+    case "-":
+      console.log(event.key);
+      break;
+    case "+":
+      console.log(event.key);
+      break;
+    case "=":
+      console.log(event.key);
+      break;
+    case ".":
+      console.log(event.key);
+      break;
+    case "0":
+      console.log(event.key);
+      break;
+    case "1":
+      console.log(event.key);
+      break;
+    case "2":
+      console.log(event.key);
+      break;
+    case "3":
+      console.log(event.key);
+      break;
+    case "4":
+      console.log(event.key);
+      break;
+    case "5":
+      console.log(event.key);
+      break;
+    case "6":
+      console.log(event.key);
+      break;
+    case "7":
+      console.log(event.key);
+      break;
+    case "8":
+      console.log(event.key);
+      break;
+    case "9":
+      console.log(event.key);
+      break;
+    default:
+      console.log("Now you've done it...");
+      break;
+  }
+});
